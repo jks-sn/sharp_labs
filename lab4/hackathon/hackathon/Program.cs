12f1c171b6d6f8e8fa67c961536c74b0112c95db
jks_sn <maxotr@mail.ru>
2024-11-13 15:23:33 +0700

    BETTER WORK

--- lab4/hackathon/hackathon/Program.cs
+++ lab4/hackathon/hackathon/Program.cs
@@ -21,28 +21,35 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        var host = Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(logging =>
+        var host = CreateHostBuilder(args).Build();
+
+        await RunApplicationAsync(host);
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        var defaultBuilder = Host.CreateDefaultBuilder(args);
+        defaultBuilder.ConfigureLogging(logging =>
             {
                 logging.ClearProviders();
                 logging.AddConsole();
                 logging.SetMinimumLevel(LogLevel.Warning);
                 logging.AddFilter("Microsoft", LogLevel.Warning);
                 logging.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
-            })
-            .ConfigureAppConfiguration((hostingContext, config) =>
+            });
+        defaultBuilder.ConfigureAppConfiguration((hostingContext, config) =>
             {
                 config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            })
-            .ConfigureServices((context, services) =>
+            });
+        defaultBuilder.ConfigureServices((context, services) =>
             {
                 services.Configure<DataLoaderOptions>(context.Configuration.GetSection("DataLoaderOptions"));
                 services.Configure<HackathonOptions>(context.Configuration.GetSection("HackathonOptions"));
                 services.Configure<HRManagerOptions>(context.Configuration.GetSection("HRManagerOptions"));
                 services.Configure<ConnectionOptions>(context.Configuration.GetSection("ConnectionOptions"));
-                
+
                 services.AddSingleton<IFileSystem, FileSystem>();
-                
+
                 services.AddSingleton<IDataLoader, DataLoader>();
                 services.AddSingleton<IHRDirector, HRDirector>();
                 services.AddSingleton<IHRManager, HRManager>();
@@ -52,9 +59,9 @@ class Program
                 services.AddSingleton<IAssignmentStrategyFactory, StrategyFactory>();
 
                 services.AddSingleton<IPreferenceGenerator, RandomPreferenceGenerator>();
-                
+
                 services.AddScoped<HackathonPrinter>();
-                
+
                 services.AddDbContext<HackathonDbContext>((serviceProvider, optionsBuilder) =>
                 {
                     var connectionOptions = serviceProvider.GetRequiredService<IOptions<ConnectionOptions>>().Value;
@@ -62,11 +69,77 @@ class Program
                         .EnableDetailedErrors(false)
                         .EnableSensitiveDataLogging(false);
                 });
-                
-                services.AddHostedService<HackathonHostedService>();
-            })
-            .Build();
-        
-        await host.RunAsync();
+            });
+        return defaultBuilder;
+    }
+
+    private static async Task RunApplicationAsync(IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+
+        var hackathonService = serviceProvider.GetRequiredService<IHackathon>();
+        var hackathonPrinter = serviceProvider.GetRequiredService<HackathonPrinter>();
+
+        while (true)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1 - Провести один хакатон со случайными предпочтениями, сохранить условия и рассчитанную гармоничность в БД");
+            Console.WriteLine("2 - Распечатать список участников, сформированные команды и рассчитанную гармоничность по идентификатору хакатона");
+            Console.WriteLine("3 - Посчитать и распечатать среднюю гармоничность по всем хакатонам в БД");
+            Console.WriteLine("0 - Выход");
+
+            Console.Write("Ваш выбор: ");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    await RunHackathonAsync(hackathonService);
+                    break;
+                case "2":
+                    await PrintHackathonInfoAsync(hackathonPrinter);
+                    break;
+                case "3":
+                    await PrintAverageHarmonicAsync(hackathonPrinter);
+                    break;
+                case "0":
+                    Console.WriteLine("Выход из программы.");
+                    return;
+                default:
+                    Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private static Task RunHackathonAsync(IHackathon hackathonService)
+    {
+        var harmonic = hackathonService.Run();
+        Console.WriteLine($"Хакатон проведён. Гармоничность = {harmonic:F2}");
+        return Task.CompletedTask;
+    }
+
+    private static Task PrintHackathonInfoAsync(HackathonPrinter hackathonPrinter)
+    {
+        Console.Write("Введите ID хакатона: ");
+        if (int.TryParse(Console.ReadLine(), out int hackathonId))
+        {
+            hackathonPrinter.PrintHackathonById(hackathonId);
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ID.");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private static Task PrintAverageHarmonicAsync(HackathonPrinter hackathonPrinter)
+    {
+        hackathonPrinter.PrintAverageHarmonic();
+        return Task.CompletedTask;
     }
 }
